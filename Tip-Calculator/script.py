cd753972f47723bd0b78d3054aea948a9b5dba0b
WarDust00 <89101458+WarDust00@users.noreply.github.com>
2022-08-14 04:35:57 -0700

    Update script.py

--- Tip-Calculator/script.py
+++ Tip-Calculator/script.py
@@ -1,6 +1,9 @@
-meal = 44.50
-tax = 6.75/100
-tip = 15/100
-meal = meal * tax
-total = meal + meal * tip
-print total
+#First we ask the user for input
+# Then we will code the bill to be changable 
+# The tip will be %20 of the total bill amount (the bill / %20 )
+# Finally we will print the amount using float to capture the whole amount (float(tip))
+
+print("How much was your bill?")
+bill = int(input())
+tip= (bill * 0.20)
+print (float (tip))
